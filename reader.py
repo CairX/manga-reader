cee5ee74c66240d90d97a4e1f51248e5a669d694
CairX <lazycairx@gmail.com>
2015-04-20 09:49:14 +0200

    Remove unused code.

--- reader.py
+++ reader.py
@@ -1,19 +1,12 @@
 import collections
 import json
 import os
-import re
 
 from config import Config
 from flask import Flask, jsonify, send_from_directory
 
 config = Config('reader.conf')
 app = Flask(__name__)
-
-
-def natural(l):
-    convert = lambda text: int(text) if text.isdigit() else text
-    alphanum = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
-    l.sort(key=alphanum)
 
 
 @app.route("/")
@@ -28,24 +21,6 @@ def test():
         entries = json.load(file)
         entries = {"entries": entries}
         return jsonify(entries)
-
-
-@app.route("/chapters")
-def chapters():
-    path = "/home/cairns/workspace/manga-downloader/library/horimiya"
-    chapters = []
-
-    for root, dirs, files in os.walk(path):
-        number = root.replace(path, '')[1:]
-        if number:
-            chapter = {
-                "number": number,
-                "pages": sorted(files)
-            }
-            chapters.append(chapter)
-
-    chapters = sorted(chapters, key=lambda k: k["number"])
-    return jsonify({"chapters": chapters})
 
 
 @app.route("/mangas")
