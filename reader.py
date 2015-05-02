0012096772d6ec79d0606c6c4a4f5ddbab15943b
CairX <lazycairx@gmail.com>
2015-05-02 11:24:03 +0200

    Document and rearrange routes.

--- reader.py
+++ reader.py
@@ -6,16 +6,42 @@ from config import Config
 from database import Database
 
 
+# ------------------------------------------------- #
+# Setup: Initiate needed objects.
+# ------------------------------------------------- #
 app = Bottle()
 config = Config('reader.conf')
 database = Database("data/reader.db")
 
 
+# ------------------------------------------------- #
+# Index: The main reader page.
+# ------------------------------------------------- #
 @app.route("/")
 def reader():
     return static_file("reader.html", root="static")
 
 
+# ------------------------------------------------- #
+# Static: Deliver JavaScript and CSS files.
+# ------------------------------------------------- #
+@app.route("/static/<filepath:path>")
+def files(filepath):
+    return static_file(filepath, root="static")
+
+
+# ------------------------------------------------- #
+# Image: The manga page images.
+# ------------------------------------------------- #
+@app.route("/images/<filepath:path>")
+def images(filepath):
+    library = os.path.join(config.string("base"), "library")
+    return static_file(filepath, root=library)
+
+
+# ------------------------------------------------- #
+# Static: Deliver JavaScript and CSS files.
+# ------------------------------------------------- #
 @app.route("/mangas")
 def mangas():
     path = os.path.join(config.string("base"), "library/")
@@ -48,17 +74,9 @@ def mangas():
     return mangas
 
 
-@app.route("/static/<filepath:path>")
-def files(filepath):
-    return static_file(filepath, root="static")
-
-
-@app.route("/images/<filepath:path>")
-def images(filepath):
-    library = os.path.join(config.string("base"), "library")
-    return static_file(filepath, root=library)
-
-
+# ------------------------------------------------- #
+# JSON: Get last reading bookmark.
+# ------------------------------------------------- #
 @app.route("/last", method=["GET"])
 def last():
     query = "SELECT * FROM reading ORDER BY saved DESC LIMIT 1"
@@ -66,12 +84,18 @@ def last():
     return {"reading": reading}
 
 
+# ------------------------------------------------- #
+# JSON: Get reading info for a given manga title.
+# ------------------------------------------------- #
 @app.route("/reading/<manga>", method=["GET"])
 def reading(manga):
     reading = database.fetchone("SELECT * FROM reading WHERE title = ?", manga)
     return {"reading": reading}
 
 
+# ------------------------------------------------- #
+# Update: Set reading info for a given manga title.
+# ------------------------------------------------- #
 @app.route("/reading/<title>/<chapter>/<page>", method=["PUT"])
 def update(title, chapter, page):
     exists = database.fetchone("SELECT 1 FROM reading WHERE title = ?", title)
@@ -92,5 +116,8 @@ def update(title, chapter, page):
     return {"manga": reading}
 
 
+# ------------------------------------------------- #
+# Main: Run the application.
+# ------------------------------------------------- #
 if __name__ == "__main__":
     run(app, host="localhost", port=5000, debug=True)
