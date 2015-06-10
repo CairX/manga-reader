22f16491ca5fb3ad38a371b9334d3dbd9fafa03e
CairX <lazycairx@gmail.com>
2015-06-10 08:46:39 +0200

    Setup now properly includes data files on install.

--- setup.py
+++ setup.py
@@ -1,47 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='manga-reader',
-    description='Local web based manga reader.',
-    keywords='manga reader',
-    version='0.1dev0',
-    author='Thomas Cairns',
-    url='https://github.com/CairX/manga-reader',
+    name="manga-reader",
+    description="Local web based manga reader.",
+    keywords="manga reader",
+    version="0.1dev0",
+    author="Thomas Cairns",
+    url="https://github.com/CairX/manga-reader",
 
     packages=find_packages(),
-    package_data={
-        "manga-reader.data": [
-            'static/ajax.js',
-            'static/script.js',
-            'static/utils.js',
-            'static/inde.html',
-            'static/style.css'
-        ]
-    },
+    package_data={"manga-reader": ["data/*"]},
+    include_package_data=True,
 
     entry_points={
-        'console_scripts': [
-            'manga-reader=manga_reader:main',
+        "console_scripts": [
+            "manga-reader=manga_reader:main",
         ]
     },
 
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Environment :: Console',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Software Development :: Documentation',
+        "Development Status :: 1 - Planning",
+        "Environment :: Console",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Documentation",
     ]
 )
-
-"""
-    data_files=[
-        ('manga-reader/data', [
-            'static/ajax.js',
-            'static/script.js',
-            'static/utils.js',
-            'static/inde.html',
-            'static/style.css'
-        ])
-    ]
-"""
